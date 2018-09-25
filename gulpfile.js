60d607e54b71bee00154fdff21a79f5828defa44
SmartEasyInvest <leovarga82@gmail.com>
2018-09-25 18:22:44 +0300

    Translation to english

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var translate = require('gulp-translator');
 const gulp = require('gulp');
 
+var options = {
+	localeDirectory: './locales/',
+	localeExt: '.yml',
+	transform: {
+		escapeQuotes: (content, dict) => content.replace(/"/g, '&quot;').replace(/\r?\n/g, ' '),
+	}
+
+}
+
 gulp.task('translate', function() {
   var translations = ['ru', 'en'];
  
   translations.forEach(function(translation){
+  	options.lang = translation;
     gulp.src('src/*.html')
       .pipe(
-        translate('./locales/' + translation + '.yml')
+        translate(options)
         .on('error', function(){
           console.error(arguments);
         })
       )
-      .pipe(gulp.dest('./build/' + translation));
+      .pipe(gulp.dest('./' + translation));
   });
 });
